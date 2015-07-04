312cc01e5d8482866bd33f12a48b4bd62b4dd520
jpws2013 <justin.poh@students.olin.edu>
2015-07-04 09:33:17 -0400

    Changed the gitignore and the environment shell script (which should not get pushed to git)

--- env.sh
+++ env.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
 USER_HOME=$(eval echo ~${SUDO_USER})
 source $USER_HOME/Harrold/catkin_ws/devel/setup.bash
+
+export ROS_IP=169.254.7.78
+export ROS_HOSTNAME=169.254.7.78
+
+export ROS_MASTER_URI=http://mast:11311
 exec "$@"
