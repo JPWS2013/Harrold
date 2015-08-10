73726b62793307ba1e1136b5eb5db6c6ecaab433
jpws2013 <justin.poh@students.olin.edu>
2015-08-09 23:25:00 -0400

    Wrote test version of arduino driver to send battery and imu data to ROS

--- catkin_ws/src/harrold_arduino_drivers/src/driver.py
+++ catkin_ws/src/harrold_arduino_drivers/src/driver.py
@@ -1,56 +1,50 @@
 #!/usr/bin/env python
-# Software License Agreement (BSD License)
-#
-# Copyright (c) 2008, Willow Garage, Inc.
-# All rights reserved.
-#
-# Redistribution and use in source and binary forms, with or without
-# modification, are permitted provided that the following conditions
-# are met:
-#
-#  * Redistributions of source code must retain the above copyright
-#    notice, this list of conditions and the following disclaimer.
-#  * Redistributions in binary form must reproduce the above
-#    copyright notice, this list of conditions and the following
-#    disclaimer in the documentation and/or other materials provided
-#    with the distribution.
-#  * Neither the name of Willow Garage, Inc. nor the names of its
-#    contributors may be used to endorse or promote products derived
-#    from this software without specific prior written permission.
-#
-# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
-# FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
-# COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
-# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
-# BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
-# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
-# ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
-# POSSIBILITY OF SUCH DAMAGE.
-#
-# Revision $Id$
 
-## Simple talker demo that published std_msgs/Strings messages
-## to the 'chatter' topic
 
+import serial
 import rospy
 from std_msgs.msg import String
 
-def talker():
-    pub = rospy.Publisher('chatter', String, queue_size=10)
-    rospy.init_node('talker', anonymous=True)
-    rate = rospy.Rate(10) # 10hz
-    while not rospy.is_shutdown():
-        hello_str = "hello world %s" % rospy.get_time()
-        rospy.loginfo(hello_str)
-        pub.publish(hello_str)
-        rate.sleep()
+class Arduino:
+
+	def __init__(self, serial_port='/dev/ttyACM0', baudrate=115200):
+		self.port=serial_port
+		self.baud=baudrate
+		self.ser_obj=serial.Serial(self.port, self.baud, timeout=1)
+		self.pub = rospy.Publisher('arduino_test', String, queue_size=10)
+
+		self.run_arduino()
+
+	def run_arduino(self):
+
+		rospy.init_node('arduino_driver', anonymous=False)
+		print "Arduino Driver is Running!"
+
+		while not rospy.is_shutdown():
+
+			message=self.ser_obj.readline()
+			result=message.strip().split(',')
+
+			if len(result)==7:
+
+## TODO: The stuff below this line needs to be written such that it publishes to the /imu/data topic 
+#        in a manner that is appropriate for what that topic expects to see (see ROS documentation)
+
+				accel=result[0:3]
+				gyro=result[3:6]
+				batt=result[6]
+
+				res=[accel, gyro, batt]
+
+				self.pub.publish(message)
+
+				#print 'Accel=', accel
+				#print 'Gyro=', gyro
+				#print 'Batt=', batt
+				#print ''
 
 if __name__ == '__main__':
     try:
-        talker()
+        Arduino()
     except rospy.ROSInterruptException:
         pass
